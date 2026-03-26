2e85c06b16ad3d2f1104de47a5d697f2f87dea65
Michael Gallagher <micjgallagher@gmail.com>
2026-03-26 03:03:15 +0000

    implemented required features

--- main.py
+++ main.py
@@ -14,6 +14,27 @@ def authenticate(users,name, password) -> User | None:
             return account
     return None
 
+def list_users(account, accounts):
+    if not account.admin: #This ensures confidentialiaty
+        print("Users do not hafe access to user list")
+        print("Please log in as an admin")
+        return
+    for x in accounts:
+        print(x.username)
+
+def change_password(account, users):
+    password = getpass("Enter your current password")
+    if authenticate(users, account.username, password) == None:
+        password("Invalid password")
+        return #This also supports confidentially
+    newpass1 = getpass("Please enter your new password")
+    newpass2 = getpass("Please confirm your new password")
+    if newpass1 != newpass2:
+        print("Passwords do not match")
+        return
+    account.password = newpass1
+    print("succees")
+
 def main():
     users = []
     users.append(User("admin", "adminpassword", True))
@@ -30,5 +51,20 @@ def main():
         print("Invalid Credentials")
         return
     print("Welcome", account.username)
+
+    #menu options
+    print("1. List Users")
+    print("2. Change Password")
+
+    option = input()
+
+    if option == "1":
+        list_users(account, users)
+    elif option =="2":
+        change_password(account, users)
+    else:
+        print("invalid option")
+
+
 
 main()
